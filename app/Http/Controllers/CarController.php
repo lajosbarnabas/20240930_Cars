c8b69c1b7b89188c4ecb9bb4d90d7963e1aca8b8
lajosbarnabas0 <lajosbarnabas0@gmail.com>
2024-10-02 15:29:52 +0200

    carcontrollerben classes létrehozása

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Storage;
 class CarController extends Controller
 {
     private $cars;
+    private $classes = [];
 
     function __construct()
     {
@@ -19,6 +20,15 @@ class CarController extends Controller
 
         $this->cars = Storage::json('cars2.json')["cars"];
         // dd($this->cars);
+
+        foreach($this->cars as $car){
+            if (isset($car["class"]) && !in_array($car["class"], $this->classes)){
+                $this->classes[] = $car["class"];
+            }
+        }
+        sort($this->classes);
+        dd($this->classes);
+
     }
 
     function showCars(){
